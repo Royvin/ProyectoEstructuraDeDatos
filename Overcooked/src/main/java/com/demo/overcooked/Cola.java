cf950c6e32574ab9de3926bdcbcf1102d40ff0de
Royvin <royvin1205@gmail.com>
2023-07-03 21:16:57 -0600

    [ClaseCola][Metodo encola]

---
+++ Overcooked/src/main/java/com/demo/overcooked/Cola.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.demo.overcooked;
+
+/**
+ *
+ * @author Royvin Arrieta M.
+ */
+public class Cola {
+    private NodoCola frente;
+    private NodoCola ultimo;
+    private int largo;
+    
+    public Cola() {
+        this.frente = null;
+        this.ultimo = null;
+        this.largo = 0;
+    }
+    
+    public void encola(NodoCola nuevoNodo){
+        if(frente == null){
+            frente = nuevoNodo;
+            ultimo = nuevoNodo;                    
+        } else{
+            ultimo.setAtras(nuevoNodo);
+            ultimo=nuevoNodo;
+        }
+        largo++;
+    }
+}
