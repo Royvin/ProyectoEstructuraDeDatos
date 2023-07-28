c486b03c950f2ded29ea5ccd5546c0936c68fa04
Royvin <royvin1205@gmail.com>
2023-07-27 20:19:13 -0600

    [PantallaJuego][Cronometro set timer 5 min]

--- Overcooked/src/main/java/com/demo/overcooked/PantallaJuego.java
+++ Overcooked/src/main/java/com/demo/overcooked/PantallaJuego.java
@@ -4,13 +4,16 @@
  */
 package com.demo.overcooked;
 
+import static java.lang.Thread.sleep;
+
 /**
  *
  * @author XPC
  */
 public class PantallaJuego extends javax.swing.JFrame {
-    int segundos=20;
-    
+    int segundos=60;
+    int minutos=5;
+    boolean estado = true;
     
 
     /**
@@ -18,32 +21,7 @@ public class PantallaJuego extends javax.swing.JFrame {
      */
     public PantallaJuego() {
         initComponents();
-        boolean estado = true;
-        this.setLocationRelativeTo(this);
-        lblCronometro.setText("20");
-        Thread hilo = new Thread(){
-            public void run(){
-                for(;;){
-                    if(estado){
-                        try{
-                            sleep(1000);
-                            if(segundos == 0){
-                                segundos = 20;
-                            }else{
-                                segundos--;
-                            }
-                            lblCronometro.setText(segundos + " ");
-                        }catch(Exception e){
-                            
-                        }
-                    }else{
-                        break;
-                        
-                    }
-                }
-            }
-        };
-    hilo.start();
+        Cronometro();
     }
 
     /**
@@ -66,11 +44,11 @@ public class PantallaJuego extends javax.swing.JFrame {
         lblCronometro.setFont(new java.awt.Font("Dialog", 0, 48)); // NOI18N
         lblCronometro.setForeground(new java.awt.Color(0, 0, 0));
         lblCronometro.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblCronometro.setText("20");
+        lblCronometro.setText("5:00");
 
         lblCuentaRegresiva.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
         lblCuentaRegresiva.setForeground(new java.awt.Color(0, 0, 0));
-        lblCuentaRegresiva.setText("Cuenta Regresiva");
+        lblCuentaRegresiva.setText("Tiempo Restante");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -85,12 +63,10 @@ public class PantallaJuego extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblCuentaRegresiva, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(lblCronometro, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(344, Short.MAX_VALUE))
+                .addComponent(lblCuentaRegresiva, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblCronometro, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(329, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -110,7 +86,35 @@ public class PantallaJuego extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-
+public void Cronometro(){
+    estado = true;
+        this.setLocationRelativeTo(this);
+        lblCronometro.setText("5:00");
+        Thread hilo = new Thread(){
+            public void run(){
+                for(;;){
+                    if(estado){
+                        try{
+                            sleep(1000);
+                            if(segundos == 0){
+                                segundos = 60;
+                                minutos--;
+                            }else{
+                                segundos--;
+                            }
+                            lblCronometro.setText(minutos + " : " +segundos);
+                        }catch(Exception e){
+                            
+                        }
+                    }else{
+                        break;
+                        
+                    }
+                }
+            }
+        };
+    hilo.start();
+}
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
